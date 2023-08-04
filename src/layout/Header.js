92686627e639ade0c0c06d26a5379bee0e5dd07a
balasahebk_nawale <balasahebknawale@gmail.com>
2023-08-04 16:46:52 +0530

    Added dynamic header logic for signin

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,20 +1,37 @@
 import classNames from "classnames";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const Header = () => {
+  const [userState, setUserState] = useState("Sign In");
   const location = useLocation();
   const navigator = useNavigate();
 
   const finalClasses = (pathname) => {
+    console.log(`pathName : ${pathname} || location : ${location.pathname}`);
+    console.log(pathname.find((p) => p === location.pathname));
     const classes = classNames(
-      "py-3 cursor-pointer text-sm text-gray-500 font-semibold border-b-4 border-b-transparent ",
+      "py-3 cursor-pointer text-sm text-gray-500 font-semibold border-b-4 ",
       {
-        "border-b-red-700 text-black": location.pathname === pathname,
+        "border-b-red-700 text-black": pathname.find(
+          (p) => p === location.pathname
+        ),
+        "border-b-transparent ": !pathname.find((p) => p === location.pathname),
       }
     );
     return classes;
   };
+  const auth = getAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserState("Profile");
+      } else {
+        setUserState("Sign In");
+      }
+    });
+  }, [auth]);
   return (
     <div className="  border-b bg-white  shadow-md">
       <header className="flex bg-white justify-between items-center max-w-7xl m-auto px-3 sticky top-0">
@@ -27,20 +44,24 @@ const Header = () => {
         </div>
         <div>
           <ul className="flex space-x-10 ">
-            <li className={finalClasses("/")} onClick={() => navigator("/")}>
+            <li className={finalClasses(["/"])} onClick={() => navigator("/")}>
               Home
             </li>
             <li
-              className={finalClasses("/offers")}
+              className={finalClasses(["/offers"])}
               onClick={() => navigator("/offers")}
             >
               Offers
             </li>
             <li
-              className={finalClasses("/sign-in")}
-              onClick={() => navigator("/sign-in")}
+              className={finalClasses(["/sign-in", "/profile"])}
+              onClick={() =>
+                userState === "Profile"
+                  ? navigator("/profile")
+                  : navigator("/sign-in")
+              }
             >
-              Sign in
+              {userState}
             </li>
           </ul>
         </div>
